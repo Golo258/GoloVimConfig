8b8f3ae3e51a77b85a65d946fc37f6e92f309566
Grzegorz Golonka <grzegorz.1.golonka@nokia.com>
2025-04-21 17:15:47 +0000

    primary configuration for nvim

---
+++ init.lua
@@ -0,0 +1,65 @@
+vim.cmd("set expandtab") 
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+    },
+  }
+}
+local opts = {
+}
+
+require("lazy").setup(plugins, opts)
+require("catppuccin").setup()
+local builtin = require('telescope.builtin')
+local treeSitterConfig = require("nvim-treesitter.configs")
+treeSitterConfig.setup(
+  {
+    ensure_installed = {
+      'c',
+      'lua',
+      'elixir',
+      'javascript',
+      'html'
+    },
+    highlight = { enable = true },
+    indent = { enable = true }
+  }
+)
+vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+vim.cmd.colorscheme "catppuccin"
