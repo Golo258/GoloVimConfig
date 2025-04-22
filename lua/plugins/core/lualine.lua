ed0bd2e53c02ff5601dfd702dca697a1a3bd3be3
Grzegorz Golonka <grzegorz.1.golonka@nokia.com>
2025-04-22 19:56:12 +0000

    resolve problem with catpuccin  config

--- lua/plugins/core/lualine.lua
+++ lua/plugins/core/lualine.lua
@@ -1,11 +1,14 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    options = {
-      icons_enabled = true,
-      theme = "dracula",
-    }
-    require("lualine").setup()
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto", -- automatycznie dopasuje do Catppuccin, jesli jest aktywny
+				icons_enabled = true,
+				section_separators = "",
+				component_separators = "|",
+			},
+		})
+	end,
 }
--- lua/plugins/themes/tokyonight.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme tokyonight-moon") -- Mozesz zmienic na: storm / night / day / moon
-  end,
-}
