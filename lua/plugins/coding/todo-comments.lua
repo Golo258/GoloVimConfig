82d9914984400fde5b6023f0e79c5b0fbb04f963
Grzegorz Golonka <grzegorz.1.golonka@nokia.com>
2025-04-22 22:02:03 +0000

    add todo comment managmenet

---
+++ lua/plugins/coding/todo-comments.lua
@@ -0,0 +1,10 @@
+return {
+	"folke/todo-comments.nvim",
+	enabled = true,
+	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+	keys = {
+		{ "<leader>ft", "<cmd>TodoTelescope<cr>" }, -- find todos
+	
+	},
+	opts = {},
+}
